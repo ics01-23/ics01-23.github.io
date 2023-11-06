2e3bba59a43e9b52c04f1d4657799c0caed9764a
hao <haoyu_t@mail.ustc.edu.cn>
2023-11-06 22:47:52 +0800

    update lab2

--- docs/.vitepress/utils/sidebar.ts
+++ docs/.vitepress/utils/sidebar.ts
@@ -27,7 +27,7 @@ export const sidebar = {
       text: '实验',
       items: [
         { text: '实验一', link: '/labs/lab1' },
-        
+        { text: '实验二', link: '/labs/lab2' },
       ],
     },
   ],
